083c3cb7c930fd99bd68c1ad05b55dac27a6ce85
alreshdy <tekela@windowslive.com>
2026-05-24 07:02:00 +0300

    firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,18 +1,12 @@
-// Firebase configuration
-// Replace with your own Firebase project config after creating one at https://console.firebase.google.com
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-```javascript
 const firebaseConfig = {
-  apiKey: "AIza...",
+  apiKey: "*********",
   authDomain: "sabita-game.firebaseapp.com",
-  databaseURL: "https://sabita-game-default-rtdb.firebaseio.com",
+  databaseURL: "*********",
   projectId: "sabita-game",
-  ...
+  storageBucket: "sabita-game.appspot.com",
+  messagingSenderId: "779594551351",
+  appId: "*********"
+};
+
+export { firebaseConfig };
+
